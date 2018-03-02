7415bf046b016353be36a76b60ec7f529858d572
wuhaisheng <wuhaisheng@binggou.com>
2018-03-02 13:53:17 +0800

    微信工具

--- src/test/java/com/wuhaisheng/test/test1/MyTest.java
+++ src/test/java/com/wuhaisheng/test/test1/MyTest.java
@@ -26,6 +26,7 @@ public class MyTest {
         String msg ="张灯结彩月儿圆，又到元宵佳节时;花灯俏俏惹人爱，汤圆香香诱人馋;缤纷烟花洒吉祥，" +
                 "喜笑颜开福无边;送份祝福表心意，情浓谊厚心舒畅;慕小谦祝你元宵节开心，幸福快乐爽歪歪!";//元宵节祝福文案
         LOG.info("好友数量"+contactList.size());
+
       /*  for (JSONObject jsonObject:contactList) {
             WechatTools.sendMsgByUserName(msg,jsonObject.getString("UserName"));
         }*/
